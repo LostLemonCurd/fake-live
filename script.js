129087b5519bd79ed32dde0b8402aebda64eceb3
lostlemoncurd <109440184+LostLemonCurd@users.noreply.github.com>
2023-03-08 20:08:37 +0100

    Added camera enabling

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-    console.log("Let's get this party started")
+const videoElement = document.getElementById('videoCamera');
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false
+}).then((stream) => {
+    videoElement.srcObject = stream;
+    videoElement.onloadedmetadata = () => {
+        videoElement.play();
+    }
+}).catch((error) => {
+    console.error('Erreur lors de l\'accès à la caméra :', error);
+});
+
+let facingMode = "environment";
+
+async function toggleCamera() {
+    facingMode = facingMode === "environment" ? "user" : "environment";
+    const constraints = {
+        video: { facingMode: { exact: facingMode } }
+    };
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    videoElement.srcObject = stream;
 }
+
+let firstTapTime = 0;
+let tappedElement = null;
+
+document.addEventListener("touchstart", (event) => {
+    const now = Date.now();
+    const timeSinceLastTap = now - firstTapTime;
+    if (timeSinceLastTap < 300 && event.target === tappedElement) {
+        toggleCamera();
+    } else {
+        firstTapTime = now;
+        tappedElement = event.target;
+    }
+});
