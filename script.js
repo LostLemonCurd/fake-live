a3b93110839fe982827c7d4f7e92c89174531238
lostlemoncurd <109440184+LostLemonCurd@users.noreply.github.com>
2023-03-09 12:27:08 +0100

    Comments generated randomly every 1 second

--- script.js
+++ script.js
@@ -36,3 +36,44 @@ document.addEventListener("touchstart", (event) => {
         tappedElement = event.target;
     }
 });
+
+fetch('https://dummyjson.com/comments')
+.then(res => res.json())
+.then(json => {
+
+    function generateComment(json) {
+        // Select the container where the comment will be added
+        const commentContainer = document.querySelector(".comment-section");
+        console.log(commentContainer);
+            const comInfo = document.createElement("div");
+            comInfo.classList.add('com-info');
+
+            const userImages = document.createElement("img");
+            userImages.setAttribute('src', 'https://xsgames.co/randomusers/avatar.php?g=male');
+
+            const userNames = document.createElement("span");
+            const userComments = document.createElement("span");
+
+            userNames.innerHTML = json.comments[i].user.username;
+            userComments.innerHTML = json.comments[i].body;
+
+            comInfo.appendChild(userNames);
+            comInfo.appendChild(userComments);
+
+            const comments = document.createElement("div");
+            comments.classList.add("comments");
+            comments.appendChild(userImages);
+            comments.appendChild(comInfo);
+
+            commentContainer.appendChild(comments);
+
+        i++;   
+
+
+        // Set a timer to call the function again every 2 seconds
+        setTimeout(() => generateComment(json), 2000);
+    }      
+    let i = 0;
+
+    generateComment(json);
+});
